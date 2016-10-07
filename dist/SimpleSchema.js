9e54a948b4aca263eaec05be76c030afa787c7c2
Curtis Layne <curtis.s.layne@gmail.com>
2016-10-07 15:05:41 -0400

    Updating build

--- dist/SimpleSchema.js
+++ dist/SimpleSchema.js
@@ -29,6 +29,10 @@ var _createClass2 = require('babel-runtime/helpers/createClass');
 
 var _createClass3 = _interopRequireDefault(_createClass2);
 
+var _deepExtend = require('deep-extend');
+
+var _deepExtend2 = _interopRequireDefault(_deepExtend);
+
 var _mongoObject = require('mongo-object');
 
 var _mongoObject2 = _interopRequireDefault(_mongoObject);
@@ -80,6 +84,47 @@ var oneOfPropsThatCanBeFunction = ['min', 'max', 'minCount', 'maxCount', 'allowe
 
 var regExpMessages = [{ exp: _regExp2.default.Email, msg: 'must be a valid email address' }, { exp: _regExp2.default.EmailWithTLD, msg: 'must be a valid email address' }, { exp: _regExp2.default.Domain, msg: 'must be a valid domain' }, { exp: _regExp2.default.WeakDomain, msg: 'must be a valid domain' }, { exp: _regExp2.default.IP, msg: 'must be a valid IPv4 or IPv6 address' }, { exp: _regExp2.default.IPv4, msg: 'must be a valid IPv4 address' }, { exp: _regExp2.default.IPv6, msg: 'must be a valid IPv6 address' }, { exp: _regExp2.default.Url, msg: 'must be a valid URL' }, { exp: _regExp2.default.Id, msg: 'must be a valid alphanumeric ID' }, { exp: _regExp2.default.ZipCode, msg: 'must be a valid ZIP code' }, { exp: _regExp2.default.Phone, msg: 'must be a valid phone number' }];
 
+var defaultMessages = {
+  initialLanguage: 'en',
+  messages: {
+    en: {
+      required: '{{label}} is required',
+      minString: '{{label}} must be at least {{min}} characters',
+      maxString: '{{label}} cannot exceed {{max}} characters',
+      minNumber: '{{label}} must be at least {{min}}',
+      maxNumber: '{{label}} cannot exceed {{max}}',
+      minNumberExclusive: '{{label}} must be greater than {{min}}',
+      maxNumberExclusive: '{{label}} must be less than {{max}}',
+      minDate: '{{label}} must be on or after {{min}}',
+      maxDate: '{{label}} cannot be after {{max}}',
+      badDate: '{{label}} is not a valid date',
+      minCount: 'You must specify at least {{minCount}} values',
+      maxCount: 'You cannot specify more than {{maxCount}} values',
+      noDecimal: '{{label}} must be an integer',
+      notAllowed: '{{value}} is not an allowed value',
+      expectedType: '{{label}} must be of type {{dataType}}',
+      regEx: function regEx(_ref) {
+        var label = _ref.label;
+        var regExp = _ref.regExp;
+
+        // See if there's one where exp matches this expression
+        var msgObj = void 0;
+        if (regExp) {
+          msgObj = _underscore2.default.find(regExpMessages, function (o) {
+            return o.exp && o.exp.toString() === regExp;
+          });
+        }
+
+        var regExpMessage = msgObj ? msgObj.msg : 'failed regular expression validation';
+
+        return label + ' ' + regExpMessage;
+      },
+
+      keyNotInSchema: '{{name}} is not allowed by the schema'
+    }
+  }
+};
+
 var SimpleSchema = function () {
   function SimpleSchema() {
     var schema = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
@@ -104,46 +149,7 @@ var SimpleSchema = function () {
     this.extend(schema);
 
     // Define default validation error messages
-    this.messageBox = new _messageBox2.default({
-      initialLanguage: 'en',
-      messages: {
-        en: {
-          required: '{{label}} is required',
-          minString: '{{label}} must be at least {{min}} characters',
-          maxString: '{{label}} cannot exceed {{max}} characters',
-          minNumber: '{{label}} must be at least {{min}}',
-          maxNumber: '{{label}} cannot exceed {{max}}',
-          minNumberExclusive: '{{label}} must be greater than {{min}}',
-          maxNumberExclusive: '{{label}} must be less than {{max}}',
-          minDate: '{{label}} must be on or after {{min}}',
-          maxDate: '{{label}} cannot be after {{max}}',
-          badDate: '{{label}} is not a valid date',
-          minCount: 'You must specify at least {{minCount}} values',
-          maxCount: 'You cannot specify more than {{maxCount}} values',
-          noDecimal: '{{label}} must be an integer',
-          notAllowed: '{{value}} is not an allowed value',
-          expectedType: '{{label}} must be of type {{dataType}}',
-          regEx: function regEx(_ref) {
-            var label = _ref.label;
-            var regExp = _ref.regExp;
-
-            // See if there's one where exp matches this expression
-            var msgObj = void 0;
-            if (regExp) {
-              msgObj = _underscore2.default.find(regExpMessages, function (o) {
-                return o.exp && o.exp.toString() === regExp;
-              });
-            }
-
-            var regExpMessage = msgObj ? msgObj.msg : 'failed regular expression validation';
-
-            return label + ' ' + regExpMessage;
-          },
-
-          keyNotInSchema: '{{name}} is not allowed by the schema'
-        }
-      }
-    });
+    this.messageBox = new _messageBox2.default(defaultMessages);
 
     // Schema-level defaults for cleaning
     this._cleanOptions = (0, _extends3.default)({
@@ -793,6 +799,11 @@ SimpleSchema.ErrorTypes = {
 SimpleSchema.Integer = 'SimpleSchema.Integer';
 SimpleSchema._makeGeneric = _mongoObject2.default.makeKeyGeneric;
 SimpleSchema.ValidationContext = _ValidationContext2.default;
+
+SimpleSchema.setDefaultMessages = function (messages) {
+  (0, _deepExtend2.default)(defaultMessages, messages);
+};
+
 function mergeSchemas(schemas) {
   var mergedSchema = {};
   _underscore2.default.each(schemas, function (schema) {
